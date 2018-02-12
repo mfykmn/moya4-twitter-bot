4ccae22023a30e6b9d4f6f8b47038ffa787461a9
mafuyuk <mafuyuk.m@gmail.com>
2018-02-12 20:33:02 +0900

    Added moya4 api client

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import json
 
 # Internal package
 from twitter_client import TwitterClient
+from moya4_api_client import Moya4APIClient
 from commands import Command
 
 def get_receiver_users(tweet, receiver_screen_name):
@@ -17,6 +18,7 @@ def get_receiver_users(tweet, receiver_screen_name):
 
 if __name__ == '__main__':
     t_client = TwitterClient()
+    m_client = Moya4APIClient()
 
     timeline = t_client.stream_bot_timeline()
     for line in timeline.iter_lines():
@@ -32,37 +34,72 @@ if __name__ == '__main__':
             if tweet_dict[0] != t_client.bot_name:
                 raise Exception("コマンド実行の形式を確認してね")
 
+            print(tweet_dict[1])
+            # コマンド:@tip_moya4_bot !開園
+            if tweet_dict[1] == Command.REGSTER.value:
+                # アドレス生成
+                m_client.register(sender_user_id, sender_user_screen_name)
+
+                # 結果をリプライ
+                res = t_client.reply(
+                    "@" + sender_user_screen_name + " TODO: !開園 コマンドの結果", tweet_id_str)
+                print(res)
             # コマンド:@tip_moya4_bot !もやたす
-            if tweet_dict[1] == Command.DEPOSIT.value:
-                print(sender_user_id)
-                print(sender_user_screen_name)
+            elif tweet_dict[1] == Command.BALANCE.value:
+                # 保持コインの確認
+                m_client.balance(sender_user_id, sender_user_screen_name)
+
+                # 結果をリプライ
                 res = t_client.reply(
                     "@" + sender_user_screen_name + " TODO: !もやたす コマンドの結果", tweet_id_str)
                 print(res)
-            # コマンド:@tip_moya4_bot !出荷 [量] [アドレス or @Twitterアカウント]
+            # コマンド:@tip_moya4_bot !種まき [数量]
+            elif tweet_dict[1] == Command.DEPOSIT.value:
+                amount = tweet_dict[2]
+
+                # コインを残高に入金する
+                m_client.deposit(sender_user_id, sender_user_screen_name, amount)
+
+                # 結果をリプライ
+                res = t_client.reply(
+                    "@" + sender_user_screen_name + " TODO: !種まき コマンドの結果", tweet_id_str)
+                print(res)
+            # コマンド:@tip_moya4_bot !収穫 [数量]
+            elif tweet_dict[1] == Command.WITHDRAW.value:
+                amount = tweet_dict[2]
+
+                # コインを残高から出金する
+                m_client.withdraw(sender_user_id, sender_user_screen_name, amount)
+
+                # 結果をリプライ
+                res = t_client.reply(
+                    "@" + sender_user_screen_name + " TODO: !収穫 コマンドの結果", tweet_id_str)
+                print(res)
+            # コマンド:@tip_moya4_bot !出荷 [メンション／アドレス] [数量]
             elif tweet_dict[1] == Command.TIP.value:
                 receiver_users = get_receiver_users(tweet, tweet_dict[2][1:])
                 amount = tweet_dict[3]
 
-                print(receiver_users)
-                print(amount)
-                print(sender_user_id)
-                print(sender_user_screen_name)
+                # コインをメンション or アドレスに送金する
+                m_client.tip(sender_user_id, sender_user_screen_name, receiver_users, amount)
 
+                # 結果をリプライ
                 res = t_client.reply(
                     "@" + sender_user_screen_name + " TODO: !出荷 コマンドの結果", tweet_id_str)
                 print(res)
-            # コマンド:@tip_moya4_bot !水やり [量]
+            # コマンド:@tip_moya4_bot !水やり [数量]
             elif tweet_dict[1] == Command.RAIN.value:
                 amount = tweet_dict[2]
 
-                print(amount)
-                print(sender_user_id)
-                print(sender_user_screen_name)
+                # 全twitterアドレスに対してコインを配布する
+                m_client.rain(sender_user_id, sender_user_screen_name, amount)
+
+                # 結果をリプライ
                 res = t_client.reply(
                     "@" + sender_user_screen_name + " TODO: !水やり コマンドの結果", tweet_id_str)
                 print(res)
             else:
+                # 結果をリプライ
                 res = t_client.reply(
                     "@" + sender_user_screen_name + " 存在しないコマンドです", tweet_id_str)
                 print(res)
---
+++ moya4_api_client.py
@@ -0,0 +1,25 @@
+class Moya4APIClient:
+    def __init__(self):
+        print("Moya4APIClient init")
+
+    def register(self, sender_user_id, sender_user_screen_name):
+        print("register")
+
+    def balance(self, sender_user_id, sender_user_screen_name):
+        print("balance")
+
+    def deposit(self, sender_user_id, sender_user_screen_name, amount):
+        print("deposit")
+
+    def withdraw(self, sender_user_id, sender_user_screen_name, amount):
+        print("withdraw")
+
+    def tip(self, sender_user_id, sender_user_screen_name, amount):
+        print("tip")
+
+    def rain(self, sender_user_id, sender_user_screen_name, amount):
+        print("rain")
+
+    def donate(self, sender_user_id, sender_user_screen_name, amount):
+        print("donate")
+
