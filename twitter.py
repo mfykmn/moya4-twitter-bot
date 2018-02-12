2492d41dca79d85a937b1c5ee2755f2017e1d7eb
mafuyuk <mafuyuk.m@gmail.com>
2018-02-12 13:58:34 +0900

    Refactor to Class

--- twitter.py
+++ twitter.py
@@ -3,58 +3,67 @@ import json
 
 from requests_oauthlib import OAuth1
 
-OAUTH = OAuth1(
-    "",
-    "",
-    "",
-    "",
-)
+class Twitter:
+    ### Private class variable ###
+    __bot_name = "tip_moya4_bot"
+    __bot_command_template = "@" + __bot_name + " [command] [twitterアカウントまたはアドレス] [amount]"
+    __oauth = OAuth1(
+        "",
+        "",
+        "",
+        "",
+    )
+    # Twitter全体のタイムラインからデータを取得するAPI
+    __public_streams_url = "https://stream.twitter.com/1.1/statuses/filter.json"
 
-BOT_NAME = "tip_moya4_bot"
+    def __init__(self):
+        print("init")
 
-def receive_tweet():
-    url = "https://stream.twitter.com/1.1/statuses/filter.json"
+    def run_worker(self):
+        print("run_worker")
+        r = requests.post(
+            self.__public_streams_url,
+            auth=self.__oauth,
+            stream=True,
+            data={"track": "@" + self.__bot_name}
+        )
 
-    r = requests.post(url, auth=OAUTH, stream=True, data={"track": "@" + BOT_NAME})
-    for line in r.iter_lines():
-        try:
-            if line == '':
-                # is not JSON. Skip
+        for line in r.iter_lines():
+            try:
+                tweet = json.loads(line.decode("utf-8"))
+                command = self.__split_command(tweet["text"])
+                print(command)
+
+
+                users = self.__get_users(tweet, command["send_destination"])
+                print(users)
+
+            except:
                 pass
 
-            tweet = json.loads(line.decode("utf-8"))
-            command = split_command(tweet["text"])
-            print(command)
+    def __split_command(self, tweet_text):
+        res = tweet_text.split(" ")
+        if res[0] != "@" + self.__bot_name:
+            raise Exception("コマンド実行の形式を確認してね")
 
+        return {
+            "command": res[1],
+            "send_destination": res[2][1:],
+            "amount": res[3]
+        }
 
-            users = get_users(tweet, command["send_destination"])
-            print(users)
+    def __get_users(self, tweet, receiver_screen_name):
+        for mention in tweet["entities"]["user_mentions"]:
+            if mention["screen_name"] == receiver_screen_name:
+                return {
+                    "sender_user_id": tweet["user"]["id"],
+                    "sender_user_screen_name": tweet["user"]["screen_name"],
+                    "receiver_id": mention["id"],
+                    "receiver_screen_name": mention["screen_name"]
+                }
 
-        except:
-            pass
-
-def split_command(tweet_text):
-    res = tweet_text.split(" ")
-    if res[0] != "@" + BOT_NAME:
-        raise Exception("fail split")
-
-    return {
-        "command": res[1],
-        "send_destination": res[2][1:],
-        "amount": res[3]
-    }
-
-def get_users(tweet, receiver_screen_name):
-    for mention in tweet["entities"]["user_mentions"]:
-        if mention["screen_name"] == receiver_screen_name:
-            return {
-                "sender_user_id": tweet["user"]["id"],
-                "sender_user_screen_name": tweet["user"]["screen_name"],
-                "receiver_id": mention["id"],
-                "receiver_screen_name": mention["screen_name"]
-            }
-
-    raise Exception("not match receiver_user")
+        raise Exception("not match receiver_user")
 
 if __name__ == '__main__':
-    receive_tweet()
+    twitter = Twitter()
+    twitter.run_worker()
