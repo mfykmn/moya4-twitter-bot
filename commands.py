e1d8a3181dff4a60343bf707031c917f5ee96a43
mafuyuk <mafuyuk.m@gmail.com>
2018-02-12 20:28:09 +0900

    Fixed Command

--- commands.py
+++ commands.py
@@ -1,10 +1,10 @@
 from enum import Enum
 
 class Command(Enum):
-    REGSTER = "！開園"
+    REGSTER = "!開園"
     BALANCE = "!もやたす"
     DEPOSIT = "!種まき"
-    WITHDRAW = "！収穫"
+    WITHDRAW = "!収穫"
     TIP = "!出荷"
     RAIN = "!水やり"
     DONATE = "!寄付"
