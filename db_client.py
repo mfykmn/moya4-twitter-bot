109578b30d936a50abd9e693f973473c1e84b4f0
mafuyuk <mafuyuk.m@gmail.com>
2018-02-25 14:31:24 +0900

    Added updateUserCultivationCoins method

--- db_client.py
+++ db_client.py
@@ -25,3 +25,12 @@ class DBClient:
                 cur.execute(query, (user_id, wallet_address))
             conn.commit()
 
+    def updateUserCultivationCoins(self, user_id, cultivation_coins):
+        print("updateUserCultivationCoins")
+        query = "update users set cultivation_coins = %s ,updated_at = now() where twitter_user_id = %s"
+
+        with self.__get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(query, (cultivation_coins, user_id))
+            conn.commit()
+
